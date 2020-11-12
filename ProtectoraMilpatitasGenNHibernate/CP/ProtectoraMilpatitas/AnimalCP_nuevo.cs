6b878b4529d5bbc57059eae90c8a29ba3e852a7d
Paula García Morales <pgm127@mscloud.ua.es>
2020-11-12 21:45:13 +0100

    AnimalCP_nuevo

--- ProtectoraMilpatitasGenNHibernate/CP/ProtectoraMilpatitas/AnimalCP_nuevo.cs
+++ ProtectoraMilpatitasGenNHibernate/CP/ProtectoraMilpatitas/AnimalCP_nuevo.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using ProtectoraMilpatitasGenNHibernate.EN.ProtectoraMilpatitas;
 using ProtectoraMilpatitasGenNHibernate.CAD.ProtectoraMilpatitas;
 using ProtectoraMilpatitasGenNHibernate.CEN.ProtectoraMilpatitas;
+using System.Linq;
 
 
 
@@ -19,31 +20,46 @@ using ProtectoraMilpatitasGenNHibernate.CEN.ProtectoraMilpatitas;
 
 namespace ProtectoraMilpatitasGenNHibernate.CP.ProtectoraMilpatitas
 {
-public partial class AnimalCP : BasicCP
-{
-public ProtectoraMilpatitasGenNHibernate.EN.ProtectoraMilpatitas.AnimalEN Nuevo (string p_nombre, int p_edad, char p_sexo, string p_centro, ProtectoraMilpatitasGenNHibernate.Enumerated.ProtectoraMilpatitas.EstadoSaludEnum p_datosMedicos, string p_caracter)
-{
-        /*PROTECTED REGION ID(ProtectoraMilpatitasGenNHibernate.CP.ProtectoraMilpatitas_Animal_nuevo) ENABLED START*/
-
-        IAnimalCAD animalCAD = null;
-        AnimalCEN animalCEN = null;
-
-        ProtectoraMilpatitasGenNHibernate.EN.ProtectoraMilpatitas.AnimalEN result = null;
-
-
-        try
+    public partial class AnimalCP : BasicCP
+    {
+        public ProtectoraMilpatitasGenNHibernate.EN.ProtectoraMilpatitas.AnimalEN Nuevo(string p_nombre, int p_edad, char p_sexo, string p_centro, ProtectoraMilpatitasGenNHibernate.Enumerated.ProtectoraMilpatitas.EstadoSaludEnum p_datosMedicos, string p_caracter)
         {
-                SessionInitializeTransaction ();
-                animalCAD = new AnimalCAD (session);
-                animalCEN = new  AnimalCEN (animalCAD);
+            /*PROTECTED REGION ID(ProtectoraMilpatitasGenNHibernate.CP.ProtectoraMilpatitas_Animal_nuevo) ENABLED START*/
+
+            IAnimalCAD animalCAD = null;
+            AnimalCEN animalCEN = null;
+
+            UsuarioCAD usuCAD = null;
+            UsuarioCEN usuCEN = null;
+
+            NotificacionCAD notiCAD = null;
+            NotificacionCEN notiCEN = null;
+            NotificacionEN notificacionEN = null;
+            MensajeCAD mensajeCAD = null;
+            MensajeCEN mensajeCEN = null;
+
+            ProtectoraMilpatitasGenNHibernate.EN.ProtectoraMilpatitas.AnimalEN result = null;
 
 
+            try
+            {
+                SessionInitializeTransaction();
+                animalCAD = new AnimalCAD(session);
+                animalCEN = new AnimalCEN(animalCAD);
 
+                usuCAD = new UsuarioCAD(session);
+                usuCEN = new UsuarioCEN(usuCAD);
+
+                notiCAD = new NotificacionCAD(session);
+                notiCEN = new NotificacionCEN(notiCAD);
+
+                mensajeCAD = new MensajeCAD(session);
+                mensajeCEN = new MensajeCEN(mensajeCAD);
 
                 int oid;
                 //Initialized AnimalEN
                 AnimalEN animalEN;
-                animalEN = new AnimalEN ();
+                animalEN = new AnimalEN();
                 animalEN.Nombre = p_nombre;
 
                 animalEN.Edad = p_edad;
@@ -56,28 +72,46 @@ public ProtectoraMilpatitasGenNHibernate.EN.ProtectoraMilpatitas.AnimalEN Nuevo 
 
                 animalEN.Caracter = p_caracter;
 
+                notificacionEN = new NotificacionEN();
+                notificacionEN.Mensaje = "Ha llegado" + animalEN.Nombre;
+
+                IList<UsuarioEN> usuarios = usuCEN.Dame_Todos(0, -1);
+
+                if (usuarios.Count() > 0)
+                {
+                    foreach (UsuarioEN usu in usuarios)
+                    {
+                        UsuarioEN usuen = usu;
+                        mensajeCEN = (MensajeCEN)usuen.MensajeChat;
+                        MensajeCP mensajeCP = new MensajeCP(session);
+                        mensajeCP = (MensajeCP)usuen.MensajeChat;
+                        MensajeEN mensajeEN = new MensajeEN();
+                        mensajeCP.Responder(mensajeEN.Id, notificacionEN.Mensaje, usuen.Email);
+                    }
+                }
+
                 //Call to AnimalCAD
 
-                oid = animalCAD.Nuevo (animalEN);
-                result = animalCAD.ReadOIDDefault (oid);
+                oid = animalCAD.Nuevo(animalEN);
+                result = animalCAD.ReadOIDDefault(oid);
 
 
 
-                SessionCommit ();
-        }
-        catch (Exception ex)
-        {
-                SessionRollBack ();
+                SessionCommit();
+            }
+            catch (Exception ex)
+            {
+                SessionRollBack();
                 throw ex;
-        }
-        finally
-        {
-                SessionClose ();
-        }
-        return result;
+            }
+            finally
+            {
+                SessionClose();
+            }
+            return result;
 
 
-        /*PROTECTED REGION END*/
-}
-}
+            /*PROTECTED REGION END*/
+        }
+    }
 }
