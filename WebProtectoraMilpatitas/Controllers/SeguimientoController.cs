faa1580fb052b1df646ea127543d6b321a238c3a
panesillo69 <alejandroalcaraz968@gmail.com>
2020-12-09 20:07:27 +0100

    Crear de Seguimiento Done

--- WebProtectoraMilpatitas/Controllers/SeguimientoController.cs
+++ WebProtectoraMilpatitas/Controllers/SeguimientoController.cs
@@ -40,17 +40,44 @@ namespace WebProtectoraMilpatitas.Controllers
         // GET: Seguimiento/Create
         public ActionResult Create()
         {
+            IList<UsuarioEN> listaUsu = new UsuarioCEN().Dame_Todos(0, -1);
+            IList<SelectListItem> UsuarioSel = new List<SelectListItem>();
+
+            foreach( UsuarioEN us in listaUsu)
+            {
+                UsuarioSel.Add(new SelectListItem { Text= us.Nombre, Value=us.Email});
+            }
+            ViewData["Usuario"] = UsuarioSel;
+
+            IList<AnimalEN> listaAni = new AnimalCEN().Dame_Todos(0, -1);
+            IList<SelectListItem> AniSel = new List<SelectListItem>();
+
+            foreach (AnimalEN an in listaAni)
+            {
+                AniSel.Add(new SelectListItem { Text = an.Nombre, Value = an.Id.ToString() });
+            }
+            ViewData["Animal"] = AniSel;
+
+            IList<ContratoAdopcionEN> listaSol = new ContratoAdopcionCEN().Dame_Todos(0, -1);
+            IList<SelectListItem> SolSel = new List<SelectListItem>();
+
+            foreach (ContratoAdopcionEN sol in listaSol)
+            {
+                SolSel.Add(new SelectListItem { Text = sol.Nombre, Value = sol.Id.ToString() });
+            }
+            ViewData["Contrato"] = SolSel;
             return View();
         }
 
         // POST: Seguimiento/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(SeguimientoViewModel segui)
         {
             try
             {
                 // TODO: Add insert logic here
-
+                SeguimientoCEN segCEN = new SeguimientoCEN();
+                segCEN.Nuevo(segui.Usuario, segui.Animal,segui.Contrato );
                 return RedirectToAction("Index");
             }
             catch
