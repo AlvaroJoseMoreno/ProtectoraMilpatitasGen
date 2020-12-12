b0e7c3b695849664057de19f86a13a4e7527ac04
Paula García Morales <pgm127@mscloud.ua.es>
2020-12-12 15:58:17 +0100

    prueba2

--- WebProtectoraMilpatitas/Controllers/SeguimientoController.cs
+++ WebProtectoraMilpatitas/Controllers/SeguimientoController.cs
@@ -12,7 +12,7 @@ using WebProtectoraMilpatitas.Models;
 
 namespace WebProtectoraMilpatitas.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class SeguimientoController : BasicController
     {
         // GET: Seguimiento
