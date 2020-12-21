d989a4f66ef61e01fc7cc826ba60444d2ea79740
ÁlvaroJoseMoreno <ajmc11@gcloud.ua.es>
2020-12-21 12:10:19 +0100

    cambios de imagen

--- WebProtectoraMilpatitas/Scripts/codigo.js
+++ WebProtectoraMilpatitas/Scripts/codigo.js
@@ -1,1 +1,11 @@
 ﻿
+function cargarFoto(inp) {
+	let filereader = new FileReader();
+	filereader.onload = function () {
+
+		inp.parentNode.querySelector('img').src = filereader.result;
+	};
+
+	filereader.readAsDataURL(inp.files[0]);
+	console.log(filereader);
+}
